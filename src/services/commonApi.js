3d8931acdf9e30fea171a82691765dc4d8920002
vyshnav9846 <vyshnav9846062099@gmail.com>
2025-01-24 13:28:09 +0530

    v2

--- src/services/commonApi.js
+++ src/services/commonApi.js
@@ -1,15 +1,15 @@
 import commonAPI from "./allApi";
 import serverUrl from "./serverURL";
 export const saveTodoApi = async (todoDetails) => {
-   return await commonAPI("POST", `${serverUrl}`, todoDetails)
+   return await commonAPI("POST", `${serverUrl}/allTodo`, todoDetails)
 
 }
 export const getAllTodoApi = async () => {
-   return await commonAPI("GET", `${serverUrl}`, "")
+   return await commonAPI("GET", `${serverUrl}/allTodo`, "")
 
 }
 export const deleteTodoApi = async (id) => {
-   return await commonAPI("DELETE", `${serverUrl}/${id}`, "")
+   return await commonAPI("DELETE", `${serverUrl}/allTodo/${id}`, "")
 
 }
 
